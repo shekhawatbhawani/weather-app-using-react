e1c14c31e6831dcd698789ff3dd1b90ddd5786ce
Shekhawatbhawanisingh <shekhawatbhwanisingh49@gmail.com>
2025-01-22 08:17:04 +0530

    weatehr app using react

---
+++ src/App.jsx
@@ -0,0 +1,114 @@
+import Card from "./Components/Card";
+import { useState, useEffect } from "react";
+
+function App() {
+  const [input, setInput] = useState("sikar");
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState("");
+  const [temp, setTemp] = useState("");
+  const [minTemp, setMinTemp] = useState("");
+  const [maxTemp, setMaxTemp] = useState("");
+  const [feelsLike, setFeelsLike] = useState("");
+  const [humidity, setHumidity] = useState("");
+  const [wind, setWind] = useState("");
+  const [pressure, setPressure] = useState("");
+  const [date, setDate] = useState("");
+  let [src ,setSrc] = useState("")
+
+  useEffect(() => {
+    fetchData();
+    time();
+  }, [input]);
+
+  const inputValue = (e) => {
+    setInput(e.target.value === "" ? "jaipur" : e.target.value);
+  };
+
+  const time = () => {
+    let date = new Date();
+    let day = date.getDay();
+    let raj = date.getDate();
+    let month = date.getMonth();
+    let year = date.getFullYear();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let time = `${hours}:${minutes}`;
+    let ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12;
+    let dayArray = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let monthArray = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let dayName = dayArray[day];
+    let monthName = monthArray[month];
+    let finalDate = `${dayName}, ${monthName} ${raj}, ${year} at ${time} ${ampm}`;
+    setDate(finalDate);
+  };
+
+  const fetchData = async () => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=b87abf895c511c59dbf662c17d489357`
+    );
+    const data = await response.json();
+
+    // Set all state variables based on the fetched data
+    setCity(data.name);
+    setWeather(data.weather[0].main);
+    setTemp((parseInt(data.main.temp) - 273.15).toFixed(2));
+    setMinTemp((parseInt(data.main.temp_min) - 273.15).toFixed(2));
+    setMaxTemp((parseInt(data.main.temp_max) - 273.15).toFixed(2));
+    setFeelsLike((parseInt(data.main.feels_like) - 273.15).toFixed(2));
+    setHumidity(data.main.humidity);
+    setWind(data.wind.speed);
+    setPressure(data.main.pressure);
+    setSrc(`https://openweathermap.org/img/wn/${data.weather?.[0]?.icon}@4x.png`)
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-gradient-to-r from-blue-100 to-indigo-400">
+      <div className="bg-white p-6 rounded-xl shadow-lg w-full md:w-[400px]">
+        <input
+          type="text"
+          placeholder="Search your city"
+          className="w-full p-3 rounded-lg mb-5 border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          onChange={inputValue}
+        />
+        <Card
+          city={city}
+          weather={weather}
+          temp={temp}
+          minTemp={minTemp}
+          maxTemp={maxTemp}
+          feelsLike={feelsLike}
+          humidity={humidity}
+          wind={wind}
+          pressure={pressure}
+          date={date}
+          src={src}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Card.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+
+const Card = (props) => {
+  let {
+    city,
+    weather,
+    temp,
+    minTemp,
+    maxTemp,
+    feelsLike,
+    humidity,
+    wind,
+    pressure,
+    date,
+    src,
+  } = props;
+
+  return (
+    <div className="max-w-sm mx-auto px-6 bg-white rounded-xl shadow-lg shadow-gray-50">
+      <h1 className="text-center text-3xl font-semibold text-gray-900">
+        {city}
+      </h1>
+      <p className="text-center text-sm text-gray-500">{date}</p>
+
+      <div className="text-center mt-4">
+        <span className="bg-blue-600 text-white font-medium px-6 py-2 rounded-full uppercase text-sm">
+          {weather}
+        </span>
+      </div>
+
+      <div className="flex justify-center items-center">
+        <img src={src} alt="Logo" className="w-20 h-20 object-contain" />
+      </div>
+
+      <h1 className="text-center text-3xl mt-2 font-semibold text-gray-800">
+        {temp}°C
+      </h1>
+      <p className="text-center text-base text-gray-600 mt-1">
+        Min: {minTemp}°C | Max: {maxTemp}°C
+      </p>
+
+      <div className="mt-3 bg-gray-30 p-4 rounded-lg shadow-md">
+        <div className="grid grid-cols-2 gap-4">
+          <div className="flex items-center justify-center space-x-2">
+            <i className="fas fa-sun text-yellow-500 text-2xl"></i>
+            <div>
+              <p className="font-medium text-gray-700">Feels Like</p>
+              <p className="text-lg font-semibold text-gray-900">
+                {feelsLike}°C
+              </p>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center space-x-2">
+            <i className="fas fa-tint text-blue-500 text-2xl"></i>
+            <div>
+              <p className="font-medium text-gray-700">Humidity</p>
+              <p className="text-lg font-semibold text-gray-900">{humidity}%</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-4 mt-4">
+          <div className="flex items-center justify-center space-x-2">
+            <i className="fa-solid fa-wind text-green-500 text-2xl"></i>
+            <div>
+              <p className="font-medium text-gray-700">Wind</p>
+              <p className="text-lg font-semibold text-gray-900">{wind} m/s</p>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-center space-x-2">
+            <i className="fas fa-tachometer-alt text-gray-700 text-2xl"></i>
+            <div>
+              <p className="font-medium text-gray-700">Pressure</p>
+              <p className="text-lg font-semibold text-gray-900">
+                {pressure} hPa
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
